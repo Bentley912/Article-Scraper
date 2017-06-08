6eff82a76357cba02f8200611552479ee9696c41
Patrick <pbentley07@gmail.com>
2017-06-07 23:38:47 -0400

    Added Button to Article Panels

--- public/assets/js/site.js
+++ public/assets/js/site.js
@@ -1,12 +1,14 @@
 $(".scrape").on("click", function(event) {
       event.preventDefault();
       scrapeQuery();
+      //hides original panel-warning div 
       $("#noneDiv").hide();
-//grab data from api/scrape   
+
 })
 
 
 function scrapeQuery(){
+    //grab data from api/scrape   
     queryURL = "/api/scrape";
     $.ajax({ url: queryURL, method: "GET" })
         // After data comes back from the request
@@ -14,19 +16,24 @@ function scrapeQuery(){
           console.log(queryURL);
           console.log(response);
           for (var i =0; i <response.length; i++){
+              //loop through results and add panels for each response 
               console.log("Div!!");
               var panel = $("<div>");
               var heading = $("<div>")
               var headingText = $("<h3>");
+              var button = $("<button>");
               panel.addClass("panel panel-primary");
               heading.addClass("panel-heading");
               headingText.addClass("panel-title");
+              button.addClass("btn btn-danger pull-right")
+              button.html("Save Article");
               headingText.html(response[i].title);
               heading.append(headingText);
+              panel.append(button);
               panel.append(heading);
               $("#panelSection").append(panel);
           }
-        })
+    })
 };
 
 
